86ec524ab2a391acd77f4f846a73c8d4ea7a0d7e
dubulingbo <dubulingbo@163.com>
2020-09-05 16:19:05 +0800

    环境搭建：配置了雪花算法主键ID生成策略

---
+++ src/main/java/edu/dublbo/generator/config/DbiConfig.java
@@ -0,0 +1,55 @@
+package edu.dublbo.generator.config;
+
+import edu.dublbo.generator.utils.SnowflakeIdWorker;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.math.NumberUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author DubLBo
+ * @since 2020-09-05 14:52
+ * i believe i can i do
+ */
+@Configuration
+public class DbiConfig {
+
+    private static final Logger logger = LoggerFactory.getLogger(DbiConfig.class);
+
+    @Value(value = "${dublbo.dbi.id-strategy.snowflake.machineId}")
+    private String machineId;
+
+    @Value(value = "${dublbo.dbi.id-strategy.snowflake.serverId}")
+    private String serverId;
+
+    /**
+     * 初始化雪花算法的bean
+     * @return 雪花算法id生成策略
+     */
+    @Bean
+    public SnowflakeIdWorker generateSnowflakeIdWorker() {
+
+        long machineIdL;
+        long serverIdL;
+
+        if (StringUtils.isEmpty(machineId) || !NumberUtils.isDigits(machineId)) {
+            machineIdL = 0L;
+        } else {
+            machineIdL = Long.parseLong(machineId);
+        }
+
+        if (StringUtils.isEmpty(serverId) || !NumberUtils.isDigits(serverId)) {
+            serverIdL = 0L;
+        } else {
+            serverIdL = Long.parseLong(serverId);
+        }
+
+        logger.info("初始化雪花算法，machineId:{},serverId:{}", machineIdL, serverIdL);
+        return new SnowflakeIdWorker(machineIdL, serverIdL);
+
+    }
+
+}
