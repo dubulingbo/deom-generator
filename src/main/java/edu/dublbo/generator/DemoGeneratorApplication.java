9579c2a7c8366f22f25b038c16cac5b86709dbe7
dubulingbo <dubulingbo@163.com>
2020-09-05 09:17:02 +0800

    init projrct

---
+++ src/main/java/edu/dublbo/generator/DemoGeneratorApplication.java
@@ -0,0 +1,13 @@
+package edu.dublbo.generator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoGeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoGeneratorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/dublbo/generator/DemoGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.dublbo.generator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoGeneratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
