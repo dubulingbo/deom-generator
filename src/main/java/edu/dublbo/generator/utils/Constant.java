6ef7a43e64dc470e3ae938ff06d43f4d0ceefd8b
dubulingbo <dubulingbo@163.com>
2020-09-05 22:56:33 +0800

    定义了一些常量

---
+++ src/main/java/edu/dublbo/generator/utils/Constant.java
@@ -0,0 +1,10 @@
+package edu.dublbo.generator.utils;
+
+/**
+ * @author DubLBo
+ * @since 2020-09-05 18:22
+ * i believe i can i do
+ */
+public class Constant {
+    public static String CURRENT_USER = "admin";
+}
