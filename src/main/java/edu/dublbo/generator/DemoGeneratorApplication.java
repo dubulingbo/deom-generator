b55a5d344c20b22487ec0d181cedf1fb34638604
dubulingbo <dubulingbo@163.com>
2020-09-06 17:09:05 +0800

    整合Mybatis

--- src/main/java/edu/dublbo/generator/DemoGeneratorApplication.java
+++ src/main/java/edu/dublbo/generator/DemoGeneratorApplication.java
@@ -1,9 +1,11 @@
 package edu.dublbo.generator;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(basePackages = "edu.dublbo.generator")
 public class DemoGeneratorApplication {
 
     public static void main(String[] args) {
